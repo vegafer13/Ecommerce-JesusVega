74bc90159ffd898ecd5c20429f66ca1043ed2441
Jesus Vega <vegafer13@gmail.com>
2022-09-01 13:04:32 -0500

    avance de proyecto, carrito de compras

--- js/index.js
+++ js/index.js
@@ -124,7 +124,7 @@ function registroNewsletter() {
 //------------- Carrito de compra
 
 //>Variables
-const baseDeDatos = [
+const productos = [
     {
         id: 1,
         nombre: "sneaker1",
@@ -198,156 +198,30 @@ function carritoDeCompras(event) {
 
 //---- //---- Añadir productos a carrito de compra
 function renderizarCarrito(){
+    // Creamos una nueva array
     const carritoDuplicados = [...new Set(carrito)];
-    carritoDuplicados.forEach(item(element){
-        
+    console.log(carritoDuplicados);
+    // Comprobamos que no este duplicado los items del nuevo array con los ID del array de productos
+    carritoDuplicados.forEach(function(item){
+        const miProducto = productos.filter(function(producto){
+            return producto.id === parseInt(item);
+        });
     });
-        
+    console.log(carritoDuplicados);
+
+    // Numero de veces que se repiten los productos
+    const numeroDeProductos = carrito.reduce((contador, itemId) => {
+        // ¿Coincide las id? Incremento el contador, en caso contrario lo mantengo
+        return itemId === item 
+
+    });
 } 
 
 
 
-
-    // const carritoSinDuplicados = [...new Set(carrito)];
-    // carritoSinDuplicados.forEach(item => {
-    //     // Obtenemos el item que necesitamos de la variable base de datos
-    //     const miItem = baseDeDatos.filter((itemBaseDatos) => {
-    //         // ¿Coincide las id? Solo puede existir un caso
-    //         return itemBaseDatos.id === parseInt(item);
-    //     });
-    // })
-    // const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-    //     // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
-    //     return itemId === item ? total += 1 : total;
-    // }, 0);
-    // // Creamos el nodo del item del carrito
-    // const miNodo = document.createElement('li');
-    // miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
-    // miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
-
-
-    // let id = baseDeDatos.find(item => item.id === carrito);
-    // console.log(id);
-
-
-}
  
  
 
 
 
 // let talla = [23,24,25,26,27];
-
-
-// ------ > Constructora de usuarios
-// function Usuario(info) {
-//     this.nombre = info.nombreUser;
-//     this.email = info.emailUser;
-//     this.direccion = info.direccionUser;
-//     this.telefono = info.telefonoUser;
-// }
-
-// let usuario = new Usuario(
-//     {
-//     nombreUser: prompt("Agrega tu nombre"),
-//     emailUser: prompt("Agrega tu correo"),
-//     direccionUser: prompt("Agrega tu correo"),
-//     telefonoUser: prompt("Agrega tu correo"), 
-// }
-// )
-
-// console.log(usuario.nombre);
-
-
-
-
-
-// //----------> Constructora de productos
-// function Producto(info) {
-//     this.id = info.idProducto;
-//     this.nombre = info.nombreProducto;
-//     this.precio = info.precioProducto;
-//     this.imagen = info.imagenProducto;
-//     this.color = info.colorProducto;
-
-// }
-
-// let producto = new Producto(
-//     {
-//     idProducto: prompt("Agrega el id del producto"),
-//     nombreProducto: prompt("Agrega el nombre del producto"),
-//     precioProducto: prompt("Agrega el precio del producto"),
-//     imagenProducto: prompt("Agrega la imagen del producto"),
-//     colorProducto: prompt("Agrega el color del producto"), 
-// }
-// )
-
-
-
-
-// let productos_array = [
-//     {
-//         id: 1,
-//         nombre: "sneaker1",
-//         precio: 700,
-//         imagen: "../assets/productos/producto-1.webp",
-//         color: "blanco",
-//     },
-//     {
-//         id: 2,
-//         nombre: "sneaker2",
-//         precio: 750,
-//         imagen: "../assets/productos/producto-2.webp",
-//         color: "negro",
-//     },
-//     {
-//         id: 3,
-//         nombre: "sneaker3",
-//         precio: 600,
-//         imagen: "../assets/productos/producto-3.webp",
-//         color: "gris",
-//     },
-//     {
-//         id: 4,
-//         nombre: "sneaker4",
-//         precio: 650,
-//         imagen: "../assets/productos/producto-4.webp",
-//         color: "cafe",
-//     },
-//     {
-//         id: 5,
-//         nombre: "sneaker5",
-//         precio: 500,
-//         imagen: "../assets/productos/producto-5.webp",
-//         color: "rojo",
-//     },
-//     {
-//         id: 6,
-//         nombre: "sneaker6",
-//         precio: 400,
-//         imagen: "../assets/productos/producto-6.webp",
-//         color: "verde",
-//     },
-//     {
-//         id: 7,
-//         nombre: "sneaker7",
-//         precio: 450,
-//         imagen: "../assets/productos/producto-7.webp",
-//         color: "rosado",
-//     },
-//     {
-//         id: 8,
-//         nombre: "sneaker8",
-//         precio: 600,
-//         imagen: "../assets/productos/producto-8.webp",
-//         color: "azul",
-//     },
-// ];
-
-// let talla_array = [23,24,25,26,27];
-// let cart = [];
-
-// function displayProducts(productos_array) {
-    
-    
-// }
