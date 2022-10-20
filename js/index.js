1f75c314ecfc7cc53e07aae30f6da6185908c387
Jesus Vega <vegafer13@gmail.com>
2022-10-20 01:19:35 -0500

    Actualizacion ruta json

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const nuevoNewsletter = [];
 
 
 const cardProductos = document.getElementById("productCard")
-fetch("./../js/productos.json")
+fetch("../productos.json")
 .then(response => response.json())
 .then(data => {
     data.forEach(producto => {
